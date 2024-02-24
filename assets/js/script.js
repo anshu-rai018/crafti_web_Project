92b5a33cd69a2cadf88d6f2122311d704c56c60a
Anshu Rai <anshu.rai018@gmail.com>
2024-02-25 04:48:59 +0530

    Crafti project first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,37 @@
  */
 
 const addEventOnElements = function(elements, eventType, callback) {
-    
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+ }
 }
+
+
+/**
+ * MOBILE NAVBAR TOGGLE
+ */
+
+const navbar =  document.querySelector("[data-navbar]");
+const navtogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function() {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+addEventOnElements(navtogglers, "click", toggleNav);
+
+
+/***
+ *  active heder when window  scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const headerActive = function (){
+    if (window.scrollY > 100) {
+        header.classList.remove("active") ;
+    }
+}
+
+window.addEventListener("scroll", headerActive);
